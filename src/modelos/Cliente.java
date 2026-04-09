3dca5f5f8baa694b5de41f43c57e9677c314c164
nerenererr <dinrss22@gmail>
2026-04-09 11:07:48 +0200

    first commit

---
+++ src/modelos/Cliente.java
@@ -0,0 +1,10 @@
+package modelos;
+
+public class Cliente {
+    private String nombre;
+    private String email;
+    private String telefono;
+    private int edad;
+    private double dinero_gastado;
+    private int productos_comprados;
+}
