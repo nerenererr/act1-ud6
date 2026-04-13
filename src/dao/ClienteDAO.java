51d157e5b90a78df572ff6a52344e3aa121083c6
nerenererr <nerenererr@gmail.com>
2026-04-13 22:14:39 +0200

    exercise 2 from j to p

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -193,4 +193,128 @@ public class ClienteDAO {
         return listaClientesMasCompra;
     }
 
+    public double obtenerSumaTotal() {
+        double suma_total = 0.0;
+        try (Connection conn = DriverManager.getConnection(url)) {
+            String sql = "SELECT SUM(dinero_gastado) AS suma_total FROM clientes";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                suma_total = rs.getDouble("suma_total");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return suma_total;
+    }
+
+    public double obtenerMedia() {
+        double media = 0.0;
+        try (Connection conn = DriverManager.getConnection(url)) {
+            String sql = "SELECT AVG(productos_comprados) AS media_productos FROM clientes";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                media = rs.getDouble("media_productos");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return media;
+    }
+
+    public double obtenerMediaMas25() {
+        double media = 0.0;
+        try (Connection conn = DriverManager.getConnection(url)) {
+            String sql = "SELECT AVG(dinero_gastado) AS media_dinero " +
+                    "FROM clientes WHERE edad > 25 " +
+                    "AND productos_comprados > 3";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                media = rs.getDouble("media_dinero");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return media;
+    }
+
+    public int obtenerNumClientesGasto100() {
+        int numClientes = 0;
+        try (Connection conn = DriverManager.getConnection(url)) {
+            String sql = "SELECT COUNT(id) AS clientes_gasto_100 " +
+                    "FROM clientes WHERE  dinero_gastado > 100";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                numClientes = rs.getInt("clientes_gasto_100");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return numClientes;
+    }
+
+    public int obtenerEdad3050() {
+        int numClientesEdad = 0;
+        try (Connection conn = DriverManager.getConnection(url)) {
+            String sql = "SELECT COUNT(id) AS clientes_edad " +
+                    "FROM clientes WHERE edad BETWEEN 30 AND 50";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                numClientesEdad = rs.getInt("clientes_edad");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return numClientesEdad;
+    }
+
+    public List<Cliente> obtenerClientesGasto200() {
+        List<Cliente> listaClientesGasto200 = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(url)) {
+            String sql = "SELECT * FROM clientes WHERE dinero_gastado > 200 " +
+                    "ORDER BY productos_comprados ASC";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                String nombre = rs.getString("nombre");
+                String email = rs.getString("email");
+                String telefono = rs.getString("telefono");
+                int edad = rs.getInt("edad");
+                double dinero_gastado = rs.getDouble("dinero_gastado");
+                int productos_comprados = rs.getInt("productos_comprados");
+                Cliente c = new Cliente(nombre, email, telefono, edad, dinero_gastado, productos_comprados);
+                listaClientesGasto200.add(c);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return listaClientesGasto200;
+    }
+
+    public Cliente obtenerClienteJovenGastoMas400() {
+        Cliente cliente = null;
+        try (Connection conn = DriverManager.getConnection(url)) {
+            String sql = "SELECT * FROM clientes WHERE dinero_gastado > 400 ORDER BY edad ASC LIMIT 1";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                String nombre = rs.getString("nombre");
+                String email = rs.getString("email");
+                String telefono = rs.getString("telefono");
+                int edad = rs.getInt("edad");
+                double dinero_gastado = rs.getDouble("dinero_gastado");
+                int productos_comprados = rs.getInt("productos_comprados");
+                cliente = new Cliente(nombre, email, telefono, edad, dinero_gastado, productos_comprados);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return cliente;
+    }
+
 }
