5a4e9e2572b1ad240714408f7dec7eae6816e355
nerenererr <dinrss22@gmail>
2026-04-14 11:30:10 +0200

    main updated

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,41 @@ public class Main {
         //System.out.println("Cliente eliminado");
 
 
+        System.out.println("");
+        System.out.println(cdao.obtenerClientePorId(7));
+
+        System.out.println("Lista de clientes: ");
+        System.out.println(cdao.obtenerClientes());
+
+        System.out.println("Lista de clientes mayores de 30 años:");
+
+
+        System.out.println("Lista de clientes con un gasto mayor a 500€");
+
+        System.out.println("Lista de clientes ordenados por dinero gastado de mayor a menor");
+
+        System.out.println("Lista de los 3 clientes con más prodcutos comprados");
+
+        System.out.println("Suma total de dinero gastado por todos los clientes: " + cdao.obtenerSumaTotal());
+
+        System.out.println("Media de productos comprados: " + cdao.obtenerMedia());
+
+        System.out.println("Media de dinero gastado por clientes mayores de 25 años \n" +
+                "que hayan comprado más de 3 productos\n: ");
+
+        System.out.println("Clientes con un gasto mayor a 100€: ");
+
+        System.out.println("Clientes con edad entre 30  50 años: ");
+
+        System.out.println("Lista de clientes con un gasto mayor a 200€ ordenados \n" +
+                "ordenados por productos comprados\n");
+
+        System.out.println("Cliente más joven con un gasto mayor a 400€: ");
+
+
+
+
+
 
 
     }
